72f80e6effb963d88bb4edc4949671779d3b2d1e
Demi Oshin <demithetechie@gmail.com>
2023-11-28 17:33:57 +0000

    code input functions

--- utils.py
+++ utils.py
@@ -1,1 +1,19 @@
 ### helpful functions to process input data :)
+
+# read input data from textfile as int in a list
+def read_input_int(textfile):
+    with open(textfile) as f:
+        input_data = [int(line) for line in f]
+    return input_data
+
+# read input data from textfile as string in a list
+def read_input_string(textfile):
+    with open(textfile) as f:
+        input_data = [line.strip() for line in f]
+    return input_data
+
+# read input data from textfile as string in a list, space separated
+def read_input_space(textfile):
+    with open(textfile) as f:
+        input_data = [i.split(" ") for i in f]
+    return input_data
