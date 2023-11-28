5c43bf4e63d9183188da250ace4f4ef0e3345de0
Demi Oshin <demithetechie@gmail.com>
2023-11-28 17:28:09 +0000

    add day1 file

---
+++ day1/day1.py
@@ -0,0 +1,18 @@
+## Day 1 Challenge
+
+def solve_day1(input_data):
+    result_part1 = part1(input_data)
+
+    result_part2 = part2(input_data)
+
+    return result_part1, result_part2
+
+def part1(input_data):
+    
+
+    return result_part1
+
+def part2(input_data):
+
+
+    return result_part2  
