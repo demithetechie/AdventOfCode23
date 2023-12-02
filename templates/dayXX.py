df97cc84ecbfed6cd2865d503b5698363bb38aa6
Demi Oshin <demithetechie@gmail.com>
2023-12-02 15:18:09 +0000

    edit template file for day XX

--- templates/dayXX.py
+++ templates/dayXX.py
@@ -1,17 +1,12 @@
 ## Day XX Template
+from ..utils import read_input_string
 
-def solve_dayXX(input_data):
-    result_part1 = part1(input_data)
+def solve_dayXX_part1(input_data):
+    # read_input_string(input_data)
 
-    result_part2 = part2(input_data)
+    return result_part1
 
-    return result_part1, result_part2
+def solve_dayXX_part2(input_data):
+    # read_input_string(input_data)
 
-def part1(input_data):
-
-    return result_part1  
-
-def part2(input_data):
-
-
-    return result_part2  
+    return result_part2
