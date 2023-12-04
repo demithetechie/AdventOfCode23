472af987d5943ef09782973dde6f6b5a5898f575
Demi Oshin <demithetechie@gmail.com>
2023-12-04 13:50:05 +0000

    edit template test file

--- templates/test_dayXX.py
+++ templates/test_dayXX.py
@@ -2,11 +2,11 @@ from utils import read_input
 from dayXX import solve_dayXX 
 
 def test_dayXX_part1():
-    input_data = read_input("dayXX.txt") 
+    input_data = read_input("../inputs/dayXX.txt") 
     expected_output = 42  
     assert solve_dayXX(input_data) == expected_output
 
 def test_dayXX_part2():
-    input_data = read_input("dayXX.txt") 
+    input_data = read_input("../inputs/dayXX.txt") 
     expected_output = 123  
     assert solve_dayXX(input_data) == expected_output
